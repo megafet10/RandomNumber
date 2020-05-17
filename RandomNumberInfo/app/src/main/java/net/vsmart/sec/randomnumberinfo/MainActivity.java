c7d609d826fdb3d00d4cd94a883fa7a768309c00
MinhBQ <megafet10@gmail.com>
2020-05-17 13:25:15 +0700

    Init new code

---
+++ RandomNumberInfo/app/src/main/java/net/vsmart/sec/randomnumberinfo/MainActivity.java
@@ -0,0 +1,22 @@
+package net.vsmart.sec.randomnumberinfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    NativeUtils mNativeUtils;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mNativeUtils = new NativeUtils(getApplication());
+        // Example of a call to a native method
+        TextView tv = findViewById(R.id.sample_text);
+        tv.setText(mNativeUtils.stringFromJNI());
+    }
+
+
+}
---
+++ RandomNumberInfo/app/src/main/java/net/vsmart/sec/randomnumberinfo/NativeUtils.java
@@ -0,0 +1,22 @@
+package net.vsmart.sec.randomnumberinfo;
+
+import android.content.Context;
+
+public class NativeUtils {
+    Context mContext;
+
+    public NativeUtils(Context context) {
+        mContext = context;
+    }
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
